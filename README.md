11102100b7bf612b64bbcd4a4398358443933804
KeyWarden <ekeeley9@gmail.com>
2022-11-10 11:51:12 +0000

    add final readme

--- README.md
+++ README.md
@@ -4,4 +4,96 @@ The Non Decimal Numbers Site exists to provide a simple and easy to use - and na
 
 Given that the most important information is the summary itself, as well as the decription of how to read and understand non decimal number systems, that information is provided on the home page, alongside some interesting trivia on an unusual type of base number system, as well as buttons to navigate to the quiz and the page of sources for information.
 
-![The Home Page]
+![The Home Page](/assets/images/home_page.png)
+
+Following this is the quiz and info pages mentioned above. All these pages are actually one page fully loaded at the start, but which is visible changes based on the buttons pressed.
+
+## Features
+
+0. The Header
+    * This is found at the top of every page, and contains the main navigation buttons.
+    * The header contains two important sections:
+       - The site title, found on the top-center of the screen.
+       - The navigation menu, found in the bottom-center of the header, underneath the title. The menu elements, Home, Quiz, and Info, can take the user to the pages of the same name. The page the user is on is outlined.
+    * The header, as shown below, tells the user clearly both what website they are currently on, as well as how many pages there are to the site, and what they contain, making it easy for the user to navigate to the content they are after.
+
+![The Header](/assets/images/nav_elements.png)
+
+1. The Home Page
+    * The Home page consists of the following elements - The Introsuction, The Instructions, and Trivia
+        - The introduction welcomes the user to the site, and provides a basic summary of what a non decimal number system is, while also reminding the user that such a number system can be useful to know depending on their field. A button is located underneath to guide the user to the Info Page if they wish to learn more.
+        - The instructions teach the user how to read and understand all base number systems, with specific notice brought to binary and hexadecimal numbers, the ones the user will be quized on. A button is located underneath to guide the user to the Quiz Page if they wish to test themselves.
+        - The trivia section, while less important, helps the suer understand how complex base number systems can get as they move further from the traditional decimal number system, whilst still holding to the underlying framework of all such systems.
+    * Therefore, the Home Page clearly lays out the information the user is most immediately in need of, whilst making clear where to go to learn more and to test their understanding.
+
+    ![The Home Page](/assets/images/home_page.png)
+
+    2. The Quiz Page
+    * The Quiz page consists of the following elements - The Quiz and The Score
+        - The quiz is a test to check their understanding of binary and hexadecimal number systems, two of the most prominent non decimal number systems. Feedback is provided to the user on how they did with each question after answering them.
+        - The score is an indicator of how well the user is doing that updates as they progress through the quiz.
+        The list of sources here serves the same purpose as that of the Home page, focusing more on the before and after of the battle, and less on the conflict itself, and the men who lead each side.
+    * To avoid problems with the quiz, the input element is validated before the user's answers are submitted, requiring the following:
+        - Something must be enterred into the space.
+        - No more than 6 characteres can be enterred into the space.
+        - Only valid characters can be enterred into the space.
+    * If any of the above criteria are not met, the answer is not submitted, allowing the user to correctly input a valid answer instead.
+
+![The Validity Alert](/assets/images/validation_alert.png)
+
+![The Quiz Page](/assets/images/quiz_page.png)
+
+3. The Info Page
+    * The Info page simply consists of one element - The List of Sources
+        - The list is divided into three sections: Binary, Hexadecimal, and Negative base numbers.
+        - Each section contains at least one source of information on non decimal number systems, providing a variety of sources for the user to use to further investigate the subject in their personal areas of interest.
+    * Therefore, the Info page provides an easily read and used resource for users seeking to get acquire more detailed information on the overall topic.
+
+![The Info Page](/assets/images/info_page.png)
+
+4. The Footer
+    * The footer is found constantly at the bottom of the screen, regardless of where the user is on the page.
+        - This ensures the resources it provides are constantly available to the user, no matter how far into a page they might be.
+    * In the center of the footer is the organisations social links, specifically to its Facebook, Twitter, and Youtube pages, providing immediate and easy access to its avenues for news and updates, as well as bonus materials.
+    * Therefore, the footer provides easy access to additional media venues for the organisation that owns the website.
+
+![The Footer](/assets/images/footer_socials.png)
+
+## Testing
+
+* The site has been tested on both Chrome and Edge browsers.
+* I have confirmed that the project is fully responsive, looks functional, and functions on multiple screen sizes via the devtools.
+* I have confirmed the various navigation tools, found throughout the site, are fully functional, and are all easy to read and understand
+* I have confirmed the quiz seems to work, as does the validation system, and have not identified any major issues with it.
+
+### Bugs
+
+* I found multiple bugs while testing this project, however most were relatively minor cases of typos and such matters, and were easily fixed.
+* I did have a problem getting the javascript to work at first, but it was swiftly found to be that I hadn't linked it properly, and have had no issues with javascript since then..
+
+### Validator
+
+* HTML
+    - No errors occured when run through the W3 validator.
+* CSS
+    - No errors occured when run through the Jigsaw validator.
+* Javascript
+    - No errors occured when run through the JSHint validator.
+
+### Unfixed Bugs
+
+There is an unresolved issue where the only way to refresh the quiz is to refresh the page. This is a holdover from when a different system was in place, but I did not have time to correct it after removing the other system, as I didn't have time to finish said system.
+
+## Deployment
+
+* The site was deployed to GitHub pages. The steps to deploy are as follows:
+    - In the GitHub repository, navigate to the Settings tab
+    - From the source section drop-down menu, select the Master Branch
+    - Once the master branch has been selected, the page will be automatically refreshed with a detailed ribbon display to indicate the successful deployment.
+
+The live link can be found here:
+
+## Credits
+
+### Content
+The code to make the social media links as well as several parts of the css were taken from the previus Portfolio Project. I also made use of the Code Institute Gitpod Template.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,13 @@
 
 //acquiring navigation elements
-let quizButton = document.getElementById('quizButton')
-let infoButton = document.getElementById('infoButton')
-let homeNav = document.getElementById('homeNav')
-let quizNav = document.getElementById('quizNav')
-let infoNav = document.getElementById('infoNav')
-let home = document.getElementById('home')
-let quiz = document.getElementById('quiz')
-let info = document.getElementById('info')
+let quizButton = document.getElementById('quizButton');
+let infoButton = document.getElementById('infoButton');
+let homeNav = document.getElementById('homeNav');
+let quizNav = document.getElementById('quizNav');
+let infoNav = document.getElementById('infoNav');
+let home = document.getElementById('home');
+let quiz = document.getElementById('quiz');
+let info = document.getElementById('info');
 
 //creating functions to manage navigation between pages
 function openHome() {
@@ -38,21 +38,21 @@ function openInfo() {
 }
 
 //creating triggers to control navigation
-homeNav.addEventListener('click', openHome)
-quizNav.addEventListener('click', openQuiz)
-infoNav.addEventListener('click', openInfo)
-quizButton.addEventListener('click', openQuiz)
-infoButton.addEventListener('click', openInfo)
+homeNav.addEventListener('click', openHome);
+quizNav.addEventListener('click', openQuiz);
+infoNav.addEventListener('click', openInfo);
+quizButton.addEventListener('click', openQuiz);
+infoButton.addEventListener('click', openInfo);
 
 //acquiring variables to control the quiz
 let question = 0;
 let answered = 0;
 let score = 0;
-let quizTitle = document.getElementById('quizTitle')
-let quizText = document.getElementById('quizText')
-let testButton = document.getElementById('testButton')
-let quizAnswer = document.getElementById('quizAnswer')
-let scoreText = document.getElementById('score')
+let quizTitle = document.getElementById('quizTitle');
+let quizText = document.getElementById('quizText');
+let testButton = document.getElementById('testButton');
+let quizAnswer = document.getElementById('quizAnswer');
+let scoreText = document.getElementById('score');
 let answerB1 = '010001';
 let answerB2 = '31';
 let answer1B3 = '36';
@@ -64,16 +64,16 @@ let answer2H3 = '10921';
 let answer1C = '00003E';
 let answer2C = '111110';
 let checkChar = '0123456789abcdefABCDEF';
-let checkTriggered = false
+let checkTriggered = false;
 
 //creating function to validate answers
 function isValid() {
     if (question === 0) {
         playQuiz();
     } else if (quizAnswer.value === '') {
-        alert('Please type something into the answer box.')
+        alert('Please type something into the answer box.');
     } else if (quizAnswer.value.length > 6) {
-        alert('None of the answers include more than 6 characters, something has gone wrong. Please double check your answer to confirm no unneeded characters are present.')
+        alert('None of the answers include more than 6 characters, something has gone wrong. Please double check your answer to confirm no unneeded characters are present.');
     } else if (question > 0 && quizAnswer.value != '') {
         for (let char of quizAnswer.value) {
             if (checkChar.includes(char)) {
@@ -100,153 +100,153 @@ function playQuiz() {
         testButton.innerHTML = `Submit`;
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 1 && quizAnswer.value === answerB1 && answered < 1) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 1 && quizAnswer.value != answerB1 && answered < 1) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was the wrong answer. The correct answer is ${answerB1}.<br><br>Remember! Each digit in binary represents a power of 2, ascending from right to left.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 1 && question === 1) {
         quizTitle.innerHTML = `Binary 2`;
         quizText.innerHTML = `What is 000110 + 011001?<br><br>Enter your answer below in decimal.`;
         testButton.innerHTML = `Submit`;
-        quizAnswer.value = ''
+        quizAnswer.value = '';
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 2 && quizAnswer.value === answerB2 && answered < 2) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 2 && quizAnswer.value != answerB2 && answered < 2) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was the wrong answer. The correct answer is ${answerB2}.<br><br>Remember! Each digit in binary represents a power of 2, ascending from right to left.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 2 && question === 2) {
         quizTitle.innerHTML = `Binary 3`;
         quizText.innerHTML = `What do you get when you multiply 000110 by 6?<br><br>Enter your answer below in either 6-digits of binary or in decimal.`;
         testButton.innerHTML = `Submit`;
-        quizAnswer.value = ''
+        quizAnswer.value = '';
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 3 && (quizAnswer.value === answer1B3 || quizAnswer.value === answer2B3) && answered < 3) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 3 && (quizAnswer.value != answer1B3 && quizAnswer.value != answer2B3) && answered < 3) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was a wrong answer. The correct answer would be either ${answer1B3} or ${answer2B3}.<br><br>Remember! Each digit in binary represents a power of 2, ascending from right to left.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 3 && question === 3) {
         quizTitle.innerHTML = `Hexadecimal 1`;
         quizText.innerHTML = `What is 0006C0 in decimal?<br><br>Enter your answer below in decimal without including commas.`;
         testButton.innerHTML = `Submit`;
-        quizAnswer.value = ''
+        quizAnswer.value = '';
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 4 && quizAnswer.value === answerH1 && answered < 4) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 4 && quizAnswer.value != answerH1 && answered < 4) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was the wrong answer. The correct answer is ${answerH1}.<br><br>Remember! Each digit in hexadecimal represents a power of 16, ascending from right to left.<br>Also, letters A through F are used to represent numbers 10 through 15 respectively.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 4 && question === 4) {
         quizTitle.innerHTML = `Hexadecimal 2`;
         quizText.innerHTML = `What is 700 - 242?<br><br>Enter your answer below in 6-digits of hexadecimal.`;
         testButton.innerHTML = `Submit`;
-        quizAnswer.value = ''
+        quizAnswer.value = '';
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 5 && quizAnswer.value.toUpperCase() === answerH2 && answered < 5) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 5 && quizAnswer.value.toUpperCase() != answerH2 && answered < 5) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was the wrong answer. The correct answer is ${answerH2}.<br><br>Remember! Each digit in hexadecimal represents a power of 16, ascending from right to left.<br>Also, letters A through F are used to represent numbers 10 through 15 respectively.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 5 && question === 5) {
         quizTitle.innerHTML = `Hexadecimal 3`;
         quizText.innerHTML = `What do you get when you divide 007FFB by 3?<br><br>Enter your answer below in either 6-digits of hexadecimal or in decimal.`;
         testButton.innerHTML = `Submit`;
-        quizAnswer.value = ''
+        quizAnswer.value = '';
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 6 && (quizAnswer.value.toUpperCase() === answer1H3 || quizAnswer.value === answer2H3) && answered < 6) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 6 && (quizAnswer.value.toUpperCase() != answer1H3 && quizAnswer.value != answer2H3) && answered < 6) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was a wrong answer. The correct answer would be either ${answer1H3} or ${answer2H3}.<br><br>Remember! Each digit in hexadecimal represents a power of 16, ascending from right to left.<br>Also, letters A through F are used to represent numbers 10 through 15 respectively.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 6 && question === 6) {
         quizTitle.innerHTML = `Binary and Hexadecimal`;
         quizText.innerHTML = `What do you get when you take the binary number 011110 and add the hexadecimal number 000020 to it?<br><br>Enter your answer below in 6-digits of either hexadecimal or binary.`;
         testButton.innerHTML = `Submit`;
-        quizAnswer.value = ''
+        quizAnswer.value = '';
         quizAnswer.style.display = 'block';
         quizAnswer.style.margin = '0 auto';
-        question++
+        question++;
     } else if (question === 7 && (quizAnswer.value.toUpperCase() === answer1C || quizAnswer.value === answer2C) && answered < 7) {
         quizTitle.innerHTML = `Correct!`;
         quizText.innerHTML = `Congratulations!<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        score++
-        scoreText.innerHTML = `Current Score: ${score}/7`
-        answered++
+        score++;
+        scoreText.innerHTML = `Current Score: ${score}/7`;
+        answered++;
     } else if (question === 7 && (quizAnswer.value.toUpperCase() != answer1C && quizAnswer.value != answer2C) && answered < 7) {
         quizTitle.innerHTML = `Incorrect!`;
         quizText.innerHTML = `Unfortunately that was a wrong answer. The correct answer would be either ${answer1C} or ${answer2C}.<br><br>Remember! Each digit in binary represents a power of 2, ascending from right to left.<br><br>Remember! Each digit in hexadecimal represents a power of 16, ascending from right to left.<br>Also, letters A through F are used to represent numbers 10 through 15 respectively.<br><br>Please click the button below to continue.`;
         testButton.innerHTML = `Next`;
         quizAnswer.style.display = 'none';
-        answered++
+        answered++;
     } else if (answered === 7) {
         quizTitle.innerHTML = `Congratulations!`;
         quizText.innerHTML = `You have now completed the quiz, and earned a final score of ${score}/7!<br><br>Thank you so much for participating. If you would like to record your score for posterity, as well as give any feedback on this site you may have, you can do so below your score.<br><br>You can also see feedback provided by other users.<br><br>Thanks once more for using our site! Have a wonderful day.`;
